2d798ab1c5f2f6275c7e19c8562a241efc641613
Sooraj M <soorajmappidachery@gmail.com>
2021-10-14 13:39:37 +0530

    user agent change

--- pahe_crawler/crawler.py
+++ pahe_crawler/crawler.py
@@ -12,7 +12,7 @@ chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36")
+chrome_options.add_argument("user-agent=Mozilla/5.0 (compatible; MSIE 9.0; Windows Phone OS 7.5; Trident/5.0; IEMobile/9.0)")
 driver = webdriver.Chrome(chrome_options=chrome_options)
 pahe_url = "https://pahe.ph/"
 print("Agent ",driver.execute_script("return navigator.userAgent"))
